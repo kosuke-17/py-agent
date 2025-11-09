05371c3094c88ee88b44dd499d1521d71b47918b
kosuke-17 <tamutamu17@icloud.com>
2025-11-09 15:34:23 +0900

    サブグラフ作成

--- agent/requirements_definition_agent.py
+++ agent/requirements_definition_agent.py
@@ -1,9 +1,11 @@
 import operator
-from typing import Annotated, Sequence, TypedDict
-from langgraph.graph import START, Pregel,StateGraph
+from typing import Annotated, Literal, Sequence, TypedDict
+from langgraph.graph import START, END, Pregel, StateGraph
 from langgraph.constants import Send
+from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageParam
 
-from agent.models import Subtask
+from agent.models import ReflectionResult, SearchOutput, Subtask
 from agent.prompts import ReqDefAgentPrompts
 from agent.settings import Settings
 
@@ -15,6 +17,21 @@ class AgentState(TypedDict):
   subtask_results: Annotated[Sequence[Subtask], operator.add]
   last_answer: str
 
+class AgentSubGraphState(TypedDict):
+  question: str
+  plan: list[str]
+  subtask: str
+  is_completed: bool
+  messages: list[ChatCompletionMessageParam]
+  challenge_count: int
+  # Sequenceは配列を意味するのでSearchOutputの2次元配列
+  # 複数のノードからtool_resultsに値を追加する際に、operator.addにより既存の値に結合される
+  tool_results: Annotated[Sequence[Sequence[SearchOutput]], operator.add]
+  refrection_results: Annotated[Sequence[Sequence[ReflectionResult]], operator.add]
+  subtask_answer: str
+
+MAX_CHALLENGE_COUNT = 3
+
 # RequirementsDefinitionAgent
 class ReqDefAgent:
   def __init__(
@@ -24,6 +41,10 @@ class ReqDefAgent:
     prompts: ReqDefAgentPrompts = ReqDefAgentPrompts()
   ) -> None:
     self.settings = settings
+    self.tools = tools
+    self.tool_map = {tool.name: tool for tool in tools}
+    self.prompts = prompts
+    self.client = OpenAI(api_key=self.settings.openai_api_key)
 
   # Pregel: 大規模グラフ処理のための計算モデル
   def create_graph(self) -> Pregel:
@@ -50,6 +71,7 @@ class ReqDefAgent:
   def crate_answer(self, state: AgentState) -> dict:
     pass
 
+  # 生成された計画数分だけサブグラフを実行することが可能
   def _should_continue_exec_subtasks(self, state: AgentState) -> list:
     return [
       Send(
@@ -62,3 +84,39 @@ class ReqDefAgent:
       )
       for idx, _ in enumerate(state["plan"])
     ]
+
+  def _create_subgraph(self) -> Pregel:
+    """
+    サブグラフを作成
+
+    Returns:
+      Pregel: サブグラフ
+    """
+    workflow = StateGraph(AgentSubGraphState)
+
+    workflow.add_node("select_tools", self.select_tools)
+    workflow.add_node("execute_tools", self.execute_tools)
+    workflow.add_node("create_subtask_andwer", self.create_subtask_answer)
+    workflow.add_node("reflect_subtask", self.reflect_subtask)
+
+    workflow.add_edge(START, "select_tools")
+    workflow.add_edge("select_tools", "execute_tools")
+    workflow.add_edge("execute_tools", "create_subtask_answer")
+    workflow.add_edge("create_subtask_answer", "reflect_subtask")
+
+    workflow.add_conditional_edges(
+      "reflect_subtask",
+      self._should_continue_exec_subtask_flow,
+      {"continue": "select_tools", "end": END}
+    )
+
+    app = workflow.complie()
+    return app
+
+  def _should_continue_exec_subtask_flow(
+      self, state: AgentSubGraphState
+    ) -> Literal["end", "continue"]:
+    if state["is_completed"] or state["challenge_count"] >= MAX_CHALLENGE_COUNT:
+      return "end"
+    else:
+      return "continue"
