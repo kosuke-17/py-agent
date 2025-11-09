1901c91220904171e613b3bb0c2946cbb7fad7f1
kosuke-17 <tamutamu17@icloud.com>
2025-11-09 13:06:26 +0900

    ファイル分割

--- agent/requirements_definition_agent.py
+++ agent/requirements_definition_agent.py
@@ -1,64 +1,11 @@
 import operator
 from typing import Annotated, Sequence, TypedDict
-from pydantic import BaseModel, Field
-from pydantic_settings import BaseSettings
 from langgraph.graph import START, Pregel,StateGraph
 from langgraph.constants import Send
-from qdrant_client.models import ScoredPoint
 
+from agent.models import Subtask
 from agent.prompts import ReqDefAgentPrompts
-
-# settings.py
-class Settings(BaseSettings):
-  openai_api_key: str
-  openai_model: str
-  openai_model: str
-
-# models.py
-class SearchOutput(BaseModel):
-  file_name: str = Field(description="The file name")  
-  content: str = Field(description="The content of the file")
-
-  @classmethod
-  def from_hit(cls, hit: dict) -> "SearchOutput":
-    return cls(
-      file_name=hit["_source"]["file_name"], content=hit["_source"]["content"]
-    )
-  
-  @classmethod
-  def from_point(cls, point: ScoredPoint) -> "SearchOutput":
-    if point.payload is None:
-      raise ValueError("Payloadがありません")
-    return cls(
-      file_name=point.payload["file_name"], content=point.payload["content"]
-    )
-
-class ToolResult(BaseModel):
-  tool_name: str = Field(..., description="ツールの名前")
-  args: str = Field(..., description="ツールの引数")
-  results: list[SearchOutput] = Field(..., description="ツールの結果")
-
-class ReflectionResult(BaseModel):
-  advice: str = Field(
-    ...,
-    description="評価がNGの場合は、別のツールを試す、別の文言でツールを試すなど、なぜNGなのかとどうしたら改善できるかを考えアドバイスを作成してください。\
-アドバイスの内容は過去のアドバイスと計画内容の他のサブタスクと重複しないようにしてください。\
-アドバイスの内容をもとにツール選択・実行からやり直します。"
-  )
-  is_completed: bool = Field(
-    ...,
-    description="ツールの実行結果と回答から、サブタスクに対して正しく回答できているかの評価結果",
-  )
-
-class Subtask(BaseModel):
-  task_name: str = Field(..., description="サブタスクの名前")
-  tool_results: list[list[ToolResult]] = Field(..., description="サブタスクの結果")
-  reflection_results: list[ReflectionResult] = Field(..., description="サブタスクの評価結果")
-  is_completed: bool = Field(..., description="サブタスクが完了しているかどうか")
-  subtask_answer: str = Field(..., description="サブタスクの回答")
-  challenge_count: int = Field(..., description="サブタスクの挑戦回数")
-
-
+from agent.settings import Settings
 
 class AgentState(TypedDict):
   question: str
@@ -68,7 +15,7 @@ class AgentState(TypedDict):
   subtask_results: Annotated[Sequence[Subtask], operator.add]
   last_answer: str
 
-# # RequirementsDefinitionAgent
+# RequirementsDefinitionAgent
 class ReqDefAgent:
   def __init__(
     self,
