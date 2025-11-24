d885d0e08a78da9813adee05b0dc28754d05b20f
kosuke-17 <tamutamu17@icloud.com>
2025-11-24 11:18:30 +0900

    DocumentとLoading Documentの学習

---
+++ learn_langchain/README.md
@@ -0,0 +1,13 @@
+# LangChain DocsのLearnに書かれている内容を学習
+
+## はじめに
+
+- LangChain agentの簡単な実装を行う
+- LangGraphを用いたLangChain agentの実装を行う
+  - カスタマイズをしたい場合は、agentsにLangGraph上で実装する
+
+- コンセプト概要の説明
+  - LangChainとLangGraphのAPIを理解するためのコンセプトを説明する
+
+https://docs.langchain.com/oss/python/learn
+  
---
+++ learn_langchain/semantic_search.py
@@ -0,0 +1,37 @@
+"""
+Semantic SearchエンジンをLangcHainで構築する.
+"""
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.documents import Document
+
+documents = [
+    Document(
+        page_content="犬は動物です。散歩が大好きです。",
+        metadata={
+            "source": "mammal-pets-doc"
+        },  # メタデータは中身の内容についての補足情報。大きなドキュメントをチャンクで分割した際に、どのドキュメントからのものかを示すために使用される。
+    ),
+    Document(
+        page_content="ねこは動物です。家にいるときは、ずっと寝ています。",
+        metadata={"source": "mammal-pets-doc"},
+    ),
+]
+
+# ドキュメントを出力
+# documents--------------------------------
+# [
+#   Document(metadata={'source': 'mammal-pets-doc'}, page_content='犬は動物です。散歩が大好きです。'),
+#   Document(metadata={'source': 'mammal-pets-doc'}, page_content='ねこは動物です。家にいるときは、ずっと寝ています。')
+# ]
+# print("documents--------------------------------")
+# print(documents)
+
+# ドキュメントを読み込む
+# @see: https://docs.langchain.com/oss/python/langchain/knowledge-base#loading-documents
+file_path = "./learn_langchain/nke-10k-2023.pdf"
+
+loader = PyPDFLoader(file_path)
+docs = loader.load()
+# print("docs--------------------------------")
+# print(docs)
